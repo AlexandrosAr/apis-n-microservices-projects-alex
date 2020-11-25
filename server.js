56c34d6ecb80b7be267db81e618994ff34c090c4
aarapidis <aarapidis@deloitte.gr>
2020-11-25 22:46:28 +0200

    Timestamp solution-main page creation

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,10 +18,56 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + '/views/timestamp.html');
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api/timestamp", (req, res) => {
+  let today = new Date()
+
+  if (today != 'Invalid Date') {
+    res.json({
+      unix: today.getTime(),
+      utc: today.toUTCString()
+    });
+  } else if (today == 'Invalid Date') {
+    res.json({
+      error: 'Invalid Date'
+    });
+  }
+
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  console.log(typeof req.params.date_string)
+  let date_string = req.params.date_string;
+  let isNum = /^\d+$/.test(date_string);
+
+  if (isNum) {
+    date_string = parseInt(req.params.date_string);
+  } else {
+    date_string = req.params.date_string;
+  }
+  let date = new Date(date_string);
+  let timestamp = date.getTime();
+  let utcTime = date.toUTCString();
+
+  if (utcTime == 'Invalid Date') {
+    res.json({
+      error: 'Invalid Date'
+    });
+  } else {
+    res.json({
+      unix: timestamp,
+      utc: utcTime
+    });
+  }
 });
 
 
